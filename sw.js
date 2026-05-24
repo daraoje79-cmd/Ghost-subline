7d08edffbf4ad8f99ecd9b687fb7d313265a2c14
daraoje79 <daraoje79@gmail.com>
2026-05-24 05:54:04 -0700

    add PWA support

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'ghostchat-v1';
+const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/images/dh.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
